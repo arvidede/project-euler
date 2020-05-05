a11725f21e251cde6f2060103968c3367ec9f336
Arvid Edenheim <arved490@student.liu.se>
2020-05-05 14:16:12 +0200

    fetched problems from project euler

---
+++ problems/fetch_problems.py
@@ -0,0 +1,15 @@
+import requests
+
+base_url = 'https://projecteuler.net/minimal='
+
+for problem_id in range(1, 714):
+    try:
+        url = base_url + str(problem_id)
+        response = requests.get(url)
+        response.raise_for_status()
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        with open('./problem_{}.md'.format(problem_id), 'w+') as f:
+            f.write(response.text)
+            f.close()
