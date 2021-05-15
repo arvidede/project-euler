462b7bf7d6696723826c7645ab73e2d506469c24
Arvid Edenheim <arvidede@icloud.com>
2021-05-15 17:09:28 +0200

    Py: Solution 26

---
+++ solutions/Python/26.py
@@ -0,0 +1,23 @@
+from utils import is_prime
+
+
+# Decimal expansion
+def reciprocal_cycle(d):
+    a, i = 1, 0
+    while True:
+        a = a * 10 % d
+        if a == 1:
+            return i
+        i += 1
+
+
+# Too avoid infinite loops
+primes = [p for p in range(1000) if is_prime(p)][3:]
+max_d = (0, 0)
+
+for d in primes:
+    cycle_len = reciprocal_cycle(d)
+    if cycle_len > max_d[1]:
+        max_d = (d, cycle_len)
+
+print(max_d)
